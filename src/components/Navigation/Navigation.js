4b864fb23f1619e2ad100028c66401a9256007b0
Craig MacKenzie <craig.mackenzie@publicis.ca>
2017-09-05 11:39:58 -0400

    * add funnel cloud logo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,22 @@
 import React from "react";
 import AppBar from "material-ui/AppBar";
 import DTToolbar from "components/Toolbar";
+import logo from "assets/images/funnel_cloud@2x.png";
+import "./Navigation.css";
+
+const styles = {
+  titleStyle: {
+    paddingLeft: 63,
+    overflow: 'hidden'
+  }
+};
 
 const Navigation = () => (
   <div>
     <AppBar
+      titleStyle={styles.titleStyle}
       showMenuIconButton={false}
-      title={<span>Funnel Cloud</span>}
+      title={<div><img src={logo} className="fc-logo"/> <span>Funnel Cloud</span></div>}
     />
     <DTToolbar />
   </div>
